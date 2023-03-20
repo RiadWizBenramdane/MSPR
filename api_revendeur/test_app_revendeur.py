7be753d31e578d2782a00170e14f25fc6524d794
Riad <113392614+RiadWizBenramdane@users.noreply.github.com>
2023-03-21 00:06:23 +0100

    Update test_app_revendeur.py

--- api_revendeur/test_app_revendeur.py
+++ api_revendeur/test_app_revendeur.py
@@ -1,26 +1,47 @@
+import json
+import pytest
 from fastapi.testclient import TestClient
 
-from app import app
+from main import app
 
 client = TestClient(app)
 
-    
-     
-# Test GET product by ID
+# Test GET /products/{product_id}
 def test_read_product():
-     response = client.get("/products/1")
-     
-     assert response.status_code == 200
-     assert response.json() == {
-           "createdAt": "2023-02-19T13:42:19.010Z",
-            "name": "Rex Bailey",
-            "details": {
-                "price": "659.00",
-                "description": "The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J",
-                "color": "red"
-            },
-            "stock": 12059,
-            "id": "1"   
-    }   
- 
+    # Make a request to the endpoint with product_id=1
+    response = client.get("/products/1")
+    # Assert that the response status code is 200 OK
+    assert response.status_code == 200
+    # Assert that the response contains a "name" field
+    assert "name" in response.json()
 
+# Test POST /products/
+def test_create_product():
+    # Define the data to send in the request
+    product_data = {"name": "Test Product", "description": "This is a test product."}
+    # Make a request to the endpoint with the product data
+    response = client.post("/products/", json=product_data)
+    # Assert that the response status code is 200 OK
+    assert response.status_code == 200
+    # Assert that the response contains an "id" field
+    assert "id" in response.json()
+
+# Test PUT /products/{product_id}
+def test_update_product():
+    # Define the data to send in the request
+    product_data = {"name": "Updated Product Name", "description": "This product has been updated."}
+    # Make a request to the endpoint with product_id=1 and the updated data
+    response = client.put("/products/1", json=product_data)
+    # Assert that the response status code is 200 OK
+    assert response.status_code == 200
+    # Assert that the response contains an "id" field
+    assert "id" in response.json()
+
+# Test DELETE /products/{product_id}
+def test_delete_product():
+    # Make a request to the endpoint with product_id=1
+    response = client.delete("/products/1")
+    # Assert that the response status code is 200 OK
+    assert response.status_code == 200
+    # Assert that the response contains an "id" field
+    assert "id" in response.json()
